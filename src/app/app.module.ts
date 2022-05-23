84ea356bd7eca090bf60ad3d1bd6bfcf77c0158f
Carolina Abreu <anacarolinaks19@gmail.com>
2022-05-23 16:59:22 -0300

    feat: component createArea created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateAreaComponent } from './components/create-area/create-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAreaComponent
   ],
   imports: [
     BrowserModule
