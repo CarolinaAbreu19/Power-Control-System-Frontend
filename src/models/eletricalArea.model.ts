35a0623cf1ca43e28eeec5ecc4ed72e9b40ab550
Carolina Abreu <anacarolinaks19@gmail.com>
2022-05-24 14:33:19 -0300

    feat: eletricalArea model created

---
+++ src/models/eletricalArea.model.ts
@@ -0,0 +1,10 @@
+export class EletricalArea {
+    /**
+     *
+     */
+    constructor(
+        public id: String,
+        public name: String,
+        public available_energy: Number
+    ) {}
+}
