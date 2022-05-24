599f8cfbf2a8278445f26bb6709ed1bcd01f899e
Carolina Abreu <anacarolinaks19@gmail.com>
2022-05-24 14:34:11 -0300

    feat: include available energy atribute to powerPlant model

--- src/models/powerPlant.model.ts
+++ src/models/powerPlant.model.ts
@@ -4,6 +4,7 @@ export class PowerPlant {
      */
     constructor(
         public id: String,
-        public name: String
+        public name: String,
+        public available_energy: Number
     ) {}
 }
