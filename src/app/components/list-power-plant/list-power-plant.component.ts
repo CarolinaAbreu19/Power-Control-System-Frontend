7786f5cc4b03164e490e3e53137602601a724877
Gilvan <gilvango.j@gmail.com>
2022-05-27 11:39:49 -0300

    feat(list-power-plant): listing of power plants through localstorage

--- src/app/components/list-power-plant/list-power-plant.component.ts
+++ src/app/components/list-power-plant/list-power-plant.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PowerPlant } from 'src/models/powerPlant.model';
 
 @Component({
   selector: 'app-list-power-plant',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-power-plant.component.css']
 })
 export class ListPowerPlantComponent implements OnInit {
-
-  constructor() { }
+  public powerPlants: PowerPlant[] =  JSON.parse(localStorage.getItem("powerPlants") || "[]");
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  remove(powerPlant: PowerPlant){
+    const indexPowerPlant  = this.powerPlants.indexOf(powerPlant);
+     if(indexPowerPlant !== -1){
+        this.powerPlants.splice(indexPowerPlant, 1);
+        const data = JSON.stringify(this.powerPlants);
+        localStorage.setItem("powerPlants", data);
+     }
+  }
+  update(){
+
   }
 
 }
